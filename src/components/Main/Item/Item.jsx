408cd1bb2203f25fc87c9b7973114e2f27e8e202
Georgi Georgiev <gosho904@gmail.com>
2022-05-24 20:06:13 +0300

    Added: additional home view card info

--- src/components/Main/Item/Item.jsx
+++ src/components/Main/Item/Item.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useState } from 'react'
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Rating from '@mui/material/Rating';
+import Button from '@mui/material/Button';
 
 import './Item.css';
 
@@ -11,7 +14,11 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.primary,
 }));
 
-const Items = ({ name, logo }) => {
+const Items = ({ name, logo, author, category, rating, downloadLink }) => {
+
+  const [ratingValue, setRatingValue] = useState(rating || 0);
+
+
   return (
     <Item className="item">
       <div className="category-preview">
@@ -20,7 +27,28 @@ const Items = ({ name, logo }) => {
         </div>
         <div className="category-info">
           <h2>{name}</h2>
+          <h3 style={{color: 'grey'}}>{author}</h3>
+          <h3 style={{color: 'grey'}}>{category}</h3>
         </div>
+        <div style={{display: 'flex', flexDirection: 'row', alignItems:'center'}}>
+        
+  <Rating
+  name="simple-controlled"
+  value={ratingValue}
+  size="medium"
+  onChange={(_, newValue) => {
+    setRatingValue(newValue);
+  }}/>
+         <div style={{fontWeight:'bold', fontSize: '1.25em'}}>{rating ?? null}</div> 
+        </div>
+      <div>
+
+
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+      <Button onClick={()=> window.open(downloadLink, "_blank")} variant="text" sx={{color: 'blue',fontWeight: 'bold', fontSize: '1.25em'}}>Get</Button>
+      <Button onClick={()=> window.open(downloadLink, "_blank")} variant="text" sx={{color: 'blue',fontWeight: 'bold', fontSize: '1.25em'}}>Details</Button>
+      </div>
       </div>
     </Item>
   );
