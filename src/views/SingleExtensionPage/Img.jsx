0e25e6ca397882298fe8db79eed8aa08df3e6f1a
Georgi Georgiev <gosho904@gmail.com>
2022-05-31 17:33:32 +0300

    Fix: image alt-s

--- src/views/SingleExtensionPage/Img.jsx
+++ src/views/SingleExtensionPage/Img.jsx
@@ -6,7 +6,7 @@ function Img(src) {
   const url = info[3] + '//' + info[4] + '.' + info[5] + '.' + info[6] + '/' + info[7] + '/' + info[8] + '/' + info[9] + '/' + info[10] + '/' + info[11] + '.' + info[12]
 
   return (
-    <img src={typeof src.src !== 'string' ? url : src.src} width={src.width} height={src.height} alt="Extension logo" />
+    <img src={typeof src.src !== 'string' ? url : src.src} width={src.width} height={src.height} alt={src.alt} />
   )
 }
 
