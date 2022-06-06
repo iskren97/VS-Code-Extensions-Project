b8c7bad58c6f8ab7695d72c761daab816cbf92d8
Iskren Gyorev <iskrengyorev@gmail.com>
2022-06-06 14:43:04 +0300

    New & Most popular sections

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,14 +12,40 @@ import Header from '../Header/Header';
 import { getAllExtensions } from '../../services/extensions.service';
 
 const Main = () => {
+  const [newAddons, setNewAddons] = useState([]);
   const [recommended, setRecommended] = useState([]);
   const [popular, setPopular] = useState([]);
+
+  const setDate = (date) => {
+    const newDate = new Date(date);
+
+    return newDate;
+  };
+
   useEffect(() => {
-    getAllExtensions().then((resp) => setRecommended(resp.slice(6, 12)));
+    getAllExtensions().then((resp) =>
+      setNewAddons(
+        resp
+          .slice(-6)
+          .map((ext) => ({ ...ext, date: setDate(ext.createdOn) }))
+          .sort((a, b) => setDate(b.createdOn) - setDate(a.createdOn))
+      )
+    );
   }, []);
 
   useEffect(() => {
-    getAllExtensions().then((resp) => setPopular(resp.slice(0, 6)));
+    getAllExtensions().then((resp) => setRecommended(resp.slice(4, 10)));
+  }, []);
+
+  useEffect(() => {
+    getAllExtensions().then((resp) =>
+      setPopular(
+        resp
+          .map((ext) => ({ ...ext, popularity: ext.downloads?.length || 0 }))
+          .sort((a, b) => b.popularity - a.popularity)
+          .slice(0, 6)
+      )
+    );
   }, []);
 
   return (
@@ -29,6 +55,41 @@ const Main = () => {
         <Intro />
 
         <Categories />
+
+        <Container
+          maxWidth="xl"
+          sx={{
+            height: 'auto',
+            marginTop: '50px',
+            paddingBottom: '50px',
+            boxShadow: '0 1px 6px rgba(0,0,0,0.2)',
+            background: 'white',
+            borderRadius: '7px',
+          }}
+        >
+          <Section category={'New'} />
+
+          <Divider sx={{ marginBottom: '30px' }} />
+
+          <Grid container direction="row" spacing={2} className="item-grid">
+            {newAddons.map((ext) => {
+              return (
+                <Grid key={ext.id} item>
+                  <Items
+                    key={ext.id}
+                    name={ext.title}
+                    logo={ext.logo}
+                    author={ext.author}
+                    category={ext.category}
+                    rating={3.8}
+                    downloadLink={ext.downloadLink}
+                    extId={ext.id}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
 
         <Container
           maxWidth="xl"
